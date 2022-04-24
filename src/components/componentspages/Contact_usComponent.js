37667dfc2bc3927d2bb72f1b33e216eb2ec6de2b
ananias augusto <ananiasjaimeaugusto@gmail.com>
2022-04-24 23:29:09 +0100

    Finalizando o contacte-nos

---
+++ src/components/componentspages/Contact_usComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './stylecomponents/Contact_usComponent.css';
+
+export default function Cantact_Uscomponent(){
+    return(
+        <React.Fragment>
+            <div className="conteinerAboutC">
+                <div className="dirC">
+                    <h1>Fale Connosco</h1>
+                    <p>Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you.</p>
+                </div>
+                <div className="borderC"></div>
+                <div className="esqC">
+                    <form method="POST">
+                        <input type="text" placeholder="O seu nome"/>
+                        
+                        <textarea placeholder="A sua mensagem"></textarea>
+
+                        <input type="submit" value="Enviar a minha mensagem"/>
+
+                    </form>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+}
--- src/pages/Contact_Us.js
+++ src/pages/Contact_Us.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Menu from "../components/componentspages/menu";
 import Rotas from "../components/Rotas";
-
+import Cantact_Uscomponent from "../components/componentspages/Contact_usComponent";
 export default function contact_us(){
     return(
         <React.Fragment>
             <Menu/>
-            <h1>contact_us</h1>
+            <Cantact_Uscomponent/>
         </React.Fragment>
     )
 }
